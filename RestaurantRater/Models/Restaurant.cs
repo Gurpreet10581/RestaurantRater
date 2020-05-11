4ef4575b1d92ac9f0276837d85681b2e52204bb7
Gurpreet Singh <er.gurpreet.2017@gmail.com>
2020-05-11 10:21:56 -0400

    Set up DB Layer

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant// this is being stored in the database created below
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int  Rating { get; set; }
+
+    }
+
+
+    public class RestaurantDbContext: DbContext//created a database and inheriting from just DbContext. This model is different then IdentityModels
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
