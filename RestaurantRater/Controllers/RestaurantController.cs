335dc3b7a170f3065a3bd42f98b41320bb4b9d70
Gurpreet Singh <er.gurpreet.2017@gmail.com>
2020-05-11 15:20:07 -0400

    Delete Functionality Added

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -38,6 +39,33 @@ namespace RestaurantRater.Controllers
             }
 
             return View(restaurant);//added the restaurant so it doesn't clears everything and instead it will take us back to the view.
+        }
+
+        //Get: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)// adding int ?=nullable
+        {
+            if (id == null)//this is if the user doesn't provide a ID# to delete
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _db.Restaurants.Find(id);//finding restaurant in the database by its key id
+            if(restaurant== null)//if restaurant doesn't exists in the data base
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);// if it does find what we looking for 
+        }
+
+        //POST: Restaurant/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
